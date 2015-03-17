3e455148e06fa65f759d96830ba20b7f2394747d
chengr <sprry9560@gmail.com>
2015-03-17 19:06:24 +0800

    https://github.com/chengr/social_neworking_p1.git

--- social_network_p1/social_network_p1.cpp
+++ social_network_p1/social_network_p1.cpp
@@ -12,6 +12,7 @@ vector <int> list[916426];
 //nodes:875713 edges:5105039
 bool find_triple(int i,int j);
 int combination(int x,int y);
+bool check_exist(int d1,int d2);
 int _tmain(int argc, _TCHAR* argv[])
 {
 	///////////////////////////////
@@ -25,11 +26,14 @@ int _tmain(int argc, _TCHAR* argv[])
 	int d1=0;
 	int d2=0;
 	int c=0;
-	for(int i=0;i<5105030;i++)
+	for(int i=0;i<5105039;i++)
 	{
 		inputFile >> d1;
 		inputFile >> d2;
-		list[d1].push_back(d2);
+		if(!check_exist(d1,d2))
+			list[d1].push_back(d2);
+		if(!check_exist(d2,d1))
+			list[d2].push_back(d1);
 		if(c>1000000){
 			c=0;
 			cout<<d1<<":"<<d2<<endl;
@@ -42,31 +46,33 @@ int _tmain(int argc, _TCHAR* argv[])
 	//¦L¥XªLµó¦ê¦C////////////////////////////////
 	//////////////////////////////////////////////
 	/*
-	fstream fp;
-    fp.open("out.txt", ios::out);
+	fstream fp2;
+    fp2.open("out.txt", ios::out);
 
 	for(int j=0;j<916426;j++){
 		if(list[j].size()>0){
 			//cout<<j<<":{";
-			fp<<j<<":{";
+			fp2<<j<<":{";
 			for(int i=0;i<list[j].size();i++){
 				//cout<<list[j][i];
-				fp<<list[j][i]<<",";
+				fp2<<list[j][i]<<",";
 			}
 			//cout<<"}"<<endl;
-			fp<<"}"<<endl;
+			fp2<<"}"<<endl;
 		}
 	}
-	fp.close();
+	fp2.close();
 	*/
 	/////////////////////////////////////////////////
 	//­pºâclustering cofficient//////////////////////
 	/////////////////////////////////////////////////
+	
 	fstream fp;
     fp.open("out2.txt", ios::out);
-
+	double sum=0;
 	for(int j=0;j<916426;j++){
 		if(list[j].size()>0){
+			double tmp=0;
 			if(list[j].size()>1){
 				double degree=(double)combination(list[j].size(),2);
 				fp<<j<<":degree:"<<degree<<",";
@@ -79,17 +85,20 @@ int _tmain(int argc, _TCHAR* argv[])
 						}
 					}
 				}
-				double tmp=triple/degree;
+				tmp=triple/degree;
 				fp<<"triple:"<<triple<<"clustering cofficient:"<<tmp<<endl;
 			}
 			else
 				fp<<j<<":degree:0,"<<"triple:0 clustering cofficient:0"<<endl;
+			sum=sum+tmp;
 		}
 
 	}
 	fp.close();
-
-
+	
+	//875711
+	cout<<sum/875711<<endl;
+	
 	system("pause");
 	return 0;
 }
@@ -106,4 +115,11 @@ int combination(int x,int y){
 		return x*(x-1)/2;
 	else
 		return 1;
+}
+bool check_exist(int d1,int d2){
+	for(int n=0;n<list[d1].size();n++){
+		if(list[d1][n]==d2)
+			return true;
+	}
+	return false;
 }
